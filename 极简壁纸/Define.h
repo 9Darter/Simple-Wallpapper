d2f07c00bc93e5fe0e61881b312f6534224a7eba
9Darter <fanwang@Mac-mini.local>
2017-02-26 23:10:51 +0800

    (1.3.0)
    Bug fixed.

--- 极简壁纸/Define.h
+++ 极简壁纸/Define.h
@@ -17,6 +17,7 @@
 #define kPathTrue1 @"http://page.appdao.com/forward?link=1988107&style=051071101&item=87044&page=1&limit=25&after=&screen_w=1125&screen_h=2001&ir=0&app=AR_Desktop&v=2.4&lang=zh-Hans-CN&it=1482498110.845991&ots=1&jb=0&as=0&mobclix=0&deviceid=replaceudid&macaddr=&idv=D43A63DE-0250-436F-947D-5B7A7ABFBAA6&idvs=&ida=F1969E49-2FC8-4DE1-8C83-324465EFBDA7&phonetype=iphone&model=iphone8%2C2&osn=iPhone%20OS&osv=10.2&tz=8"
 //旧的 每次不随机http://page.appdao.com/forward?link=1988107&style=051071101&item=91396&page=1&limit=25&after=&screen_w=1125&screen_h=2001&ir=0&app=AR_Desktop&v=2.4&lang=zh-Hans-CN&it=1481810734.391166&ots=4&jb=0&as=0&mobclix=0&deviceid=replaceudid&macaddr=&idv=08498196-57B4-4962-871D-07BB7BEE660A&idvs=&ida=F1969E49-2FC8-4DE1-8C83-324465EFBDA7&phonetype=iphone&model=iphone8%2C2&osn=iPhone%20OS&osv=10.2&tz=8
 
+//            http://page.appdao.com/forward?link=1988107&style=051071101&item=91396&page=1&limit=25&after=&screen_w=1125&screen_h=2001&ir=0&app=AR_Desktop&v=2.4&lang=zh-Hans-CN&it=1482646759.342157&ots=85&jb=0&as=0&mobclix=0&deviceid=replaceudid&macaddr=&idv=E7D08F17-0885-4670-A971-CFB222152345&idvs=&ida=F1969E49-2FC8-4DE1-8C83-324465EFBDA7&phonetype=iphone&model=iphone8%2C2&osn=iPhone%20OS&osv=10.2.1&tz=8
 
 
 
--- 极简壁纸/NetManager/BaseNetmanager.m
+++ 极简壁纸/NetManager/BaseNetmanager.m
@@ -8,11 +8,30 @@
 
 #import "BaseNetmanager.h"
 
+static BaseNetmanager *afnSingleton = nil;
+
+@interface BaseNetmanager ()
+
+@property (nonatomic, strong) AFHTTPSessionManager *manager;
+
+@end
+
 @implementation BaseNetmanager
+
++ (BaseNetmanager *)shareBaseNetManager {
+    @synchronized (self) {
+        if (afnSingleton == nil) {
+            afnSingleton = [[super allocWithZone:nil]init];
+            afnSingleton.manager = [AFHTTPSessionManager manager];
+        }
+    }
+    return afnSingleton;
+}
+
 + (id)GET:(NSString *)path parameters:(NSDictionary *)params completionHandler:(void (^)(id, NSError *))completionHandler{
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html", @"application/json", @"text/json", @"text/javascript", @"text/plain", nil];
-    return [manager GET:path parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+    //AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [self shareBaseNetManager].manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html", @"application/json", @"text/json", @"text/javascript", @"text/plain", nil];
+    return [[self shareBaseNetManager].manager GET:path parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         //NSLog(@"%@", task.currentRequest.URL.absoluteString);
         !completionHandler?:completionHandler(responseObject, nil);
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
@@ -21,4 +40,6 @@
         !completionHandler ?: completionHandler(nil, error);
     }];
 }
+
+
 @end
